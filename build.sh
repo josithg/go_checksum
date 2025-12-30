2d4e4b0f8290364045f06def81019dbbf3c97ab3
Josith Ganesan <143208179+josithg@users.noreply.github.com>
2025-12-30 12:56:35 +0530

    Create build.sh
    Add a script to install Golang based on the package manager. and to compile from source

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if command -v apt >/dev/null 2>&1; then
+	sudo apt install golang
+elif command -v dnf >/dev/null 2>&1; then
+	sudo dnf install golang 
+elif command -v pacman >dev/null 2>&1; then 
+	sudo pacman -Sy go 
+elif command -v zypper >dev/null 2>&1; then 
+	sudo zypper install go 
+elif command -v apk >dev/null 2>&1; then
+	sudo apk add go
+else
+	unable to identify distro install golang manually 
+git clone https://github.com/josithg/go_checksum.git
+cd go_checksum
+go mod tidy 
+go build -o main 
+./main
