8ad46e81cc21213e5839114258199538e46924be
Josith Ganesan <143208179+josithg@users.noreply.github.com>
2026-01-09 22:02:51 +0530

    Fix formatting of hash output messages

--- main.go
+++ main.go
@@ -20,15 +20,15 @@ func main() {
 	fmt.Print("enter your file path: ")
 	fmt.Scanln(&file)
 
-	fmt.Println("	Press 1 for SHA256		Press 2 for SHA1 	 Press 3 for MD5")
+	fmt.Println("	Press 1 for SHA256		  Press 2 for SHA1 		  Press 3 for MD5")
 	fmt.Print("enter your choice: ")
 	fmt.Scanln(&opt)
 
 	switch opt {
 	case 1:
 		s256, _ := checksum.SHA256sum(file)
-		fmt.Println("the SHA256 sum is: ", s256)
-		fmt.Print("enter your hash to compare : ")
+		fmt.Println("the SHA256 sum is          : ", s256)
+		fmt.Print("enter your hash to compare :  ")
 		fmt.Scanln(&oh)
 		if oh == s256 {
 			fmt.Println()
@@ -39,8 +39,8 @@ func main() {
 		}
 	case 2:
 		s1, _ := checksum.SHA1sum(file)
-		fmt.Println("the SHA1 sum is: ", s1)
-		fmt.Print("enter your hash to compare : ")
+		fmt.Println("the SHA1 sum is	           : ", s1)
+		fmt.Print("enter your hash to compare :  ")
 		fmt.Scanln(&oh)
 		if oh == s1 {
 			fmt.Println()
@@ -51,8 +51,8 @@ func main() {
 		}
 	case 3:
 		m5, _ := checksum.MD5sum(file)
-		fmt.Println(" the MD5 sum is: ", m5)
-		fmt.Print("enter your hash to compare : ")
+		fmt.Println(" the MD5 sum is		   : ", m5)
+		fmt.Print("enter your hash to compare :  ")
 		fmt.Scanln(&oh)
 		if oh == m5 {
 			fmt.Println("hash matched ")
